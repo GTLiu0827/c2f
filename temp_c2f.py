eabaa7c11a1376dc19e747d673759e4a6d5a19fb
David Liu <fly82827@gmail.com>
2021-05-29 13:02:24 +0800

    temperature transform test

---
+++ temp_c2f.py
@@ -0,0 +1,7 @@
+# Test 1 
+# 攝氏/華氏溫定轉換
+# User 輸入攝氏溫度 
+# 程式轉換成華氏溫度並印出
+cel_temp = input ('Pslease enter Celsius Temperature : ')
+fah_temp = (float(cel_temp) * 9 / 5) + 32
+print ('Fahrenhiet Temperature is ', fah_temp)
